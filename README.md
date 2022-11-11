921b3062187419fc18411b56bd1a38370c32ff36
robertosixty1 <roberto011herminegildo@gmail.com>
2022-11-11 14:52:32 -0300

    Arch Linux support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Setup Setup
 
-A script that configures [Ubuntu](https://archlinux.org) for my needs.
+A script that configures [Arch Linux](https://archlinux.org) and [Ubuntu](https://ubuntu.com) for my needs.
 
 ## Quick Start
 ```console
--- install.py
+++ install.py
@@ -2,6 +2,7 @@
 
 from os import getenv, listdir
 from subprocess import run
+from sys import argv
 
 USER = listdir("/home")[0]
 HOME = getenv("HOME")
@@ -13,7 +14,10 @@ if getenv("USER") == "root":
 run(["git", "clone", "https://github.com/robertosixty1/nvim", f"{HOME}/.config/nvim"])
 
 run(["git", "clone", "https://github.com/robertosixty1/programs", f"{HOME}/.config/programs"])
-run(["python", f"{HOME}/.config/programs/install.py"], cwd=f"{HOME}/.config/programs")
+if argv[1] == "arch":
+    run(["python", f"{HOME}/.config/programs/install.py", "arch"], cwd=f"{HOME}/.config/programs")
+else:
+    run(["python", f"{HOME}/.config/programs/install.py"], cwd=f"{HOME}/.config/programs")
 
 run(["git", "clone", "https://github.com/robertosixty1/shells", f"{HOME}/.config/shells"])
 run(["python", f"{HOME}/.config/shells/install.py"], cwd=f"{HOME}/.config/shells")
